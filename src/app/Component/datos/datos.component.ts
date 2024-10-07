bad3adf0f670d4c5c4d300020a55d70b82341711
CGermanSoto <c.germansoto@gmail.com>
2024-10-07 19:18:27 -0300

    Feature: Se agrega componentes, estilo, imagen

---
+++ src/app/Component/datos/datos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datos',
+  standalone: true,
+  imports: [],
+  templateUrl: './datos.component.html',
+  styleUrl: './datos.component.css'
+})
+export class DatosComponent {
+
+}
